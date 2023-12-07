92552c939103c9c275357faa62d53810ff3676f3
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-07 14:38:00 +0300

    [new]: casts a string to a float

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module takes a float as argument and returns a string
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    takes a float and returns the string representation
+    """
+    return str(n)
