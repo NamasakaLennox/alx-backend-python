6ce9f2b23ed29d225d87b7250d37b1e64befadc9
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-07 14:30:28 +0300

    [new]: concatenates two strings, enforced parameters as strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+A module that defines a function that concatenates two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    concatenates two strings passed as parameter
+    Args:
+        str1: string
+        str2: string
+    Return:
+        returns a string
+    """
+    return str1 + str2
