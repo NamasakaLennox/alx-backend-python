586d7f22f8b8577d3b14ddc4f140b1c97cad5998
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-07 14:34:48 +0300

    [new] cast a float to return the floor value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module that returns the floor of a float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    takes a float as argument and rounds it down
+    """
+    return int(n)
