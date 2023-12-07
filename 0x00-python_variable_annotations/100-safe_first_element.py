a4dbb95520693b4b83acb1825594a792dd74c1b1
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-07 16:00:30 +0300

    [new]: corect annotations

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -3,11 +3,13 @@
 augment the given code
 """
 from typing import Any, Sequence, Union
-from types import NoneType
 
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    """
+    returns the correct duck type notations
+    """
     if lst:
         return lst[0]
     else:
