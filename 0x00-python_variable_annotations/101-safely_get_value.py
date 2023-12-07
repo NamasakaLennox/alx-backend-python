e7602160d5d62f27198dd137e8a6c363c8e9d456
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-07 16:19:30 +0300

    [new]: used typevar to define ~T'

---
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+add type annotations to given function
+"""
+from typing import Any, Mapping, TypeVar, Union
+T = TypeVar("T")
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None
+                     ) -> Union[Any, T]:
+    """
+    safely returns a value
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
