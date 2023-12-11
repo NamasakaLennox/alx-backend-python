803784f5613e2438cd910ab1553f5aac74bf2d78
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-12 01:07:15 +0300

    [fix]: used time instead of delay

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -2,6 +2,7 @@
 """
 Module that measures the runtime of a concurrent process
 """
+from time import time
 import asyncio
 
 wait_n = __import__('1-concurrent_coroutines').wait_n
@@ -11,5 +12,8 @@ def measure_time(n: int, max_delay: int):
     """
     returns the maximum time taken to perform the operation
     """
-    time = max(asyncio.run(wait_n(n, max_delay)))
-    return time / n
+    start = time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time()
+
+    return (end - start) / n
