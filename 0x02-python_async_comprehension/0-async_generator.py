cb7fe41a7dec28431fb1cda03e1b9d80a7b90607
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-13 22:13:47 +0300

    [new]: async generator function

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+loops 10 times and yields a random number between 0 and 10
+"""
+import asyncio
+from random import random
+from typing import AsyncGenerator
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    an async generator, yields a value after every loop
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random() * 10
