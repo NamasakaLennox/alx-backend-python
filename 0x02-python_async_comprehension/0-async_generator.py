223a776de0b4d22c041c7a523ab189827a0da42e
NamasakaLennox <namasakalennox254@gmail.com>
2023-12-13 22:17:12 +0300

    [fix]: fixed return type to generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,9 @@ loops 10 times and yields a random number between 0 and 10
 """
 import asyncio
 from random import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     an async generator, yields a value after every loop
     """
