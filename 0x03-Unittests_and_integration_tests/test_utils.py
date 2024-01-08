8afd7191e0bc65f386d921b96c457d1bbf01551a
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-08 19:02:01 +0300

    [new]:parametize and patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -52,3 +52,31 @@ class TestGetJson(unittest.TestCase):
 
         self.assertEqual(utils.get_json(url), payload)
         mock_requests.get.assert_called_once()
+
+
+class TestMemoize(unittest.TestCase):
+    """A test calss for the memoize decorator function
+    """
+    def test_mock(self):
+        """A test method for the memoize function
+        """
+        class TestClass:
+            """Test class for memoize function
+            """
+            def a_method(self):
+                """test method
+                """
+                return 42
+
+            @utils.memoize
+            def a_property(self):
+                """Call the function to see how many times it's called
+                """
+                return self.a_method()
+
+        with mock.patch.object(TestClass, 'a_method', return_value=42) as temp:
+            test = TestClass()
+            self.assertEqual(test.a_property, 42)
+            self.assertEqual(test.a_property, 42)
+
+        temp.assert_called_once_with()
