ecaf58e69cdc8d06636cc43697cf5b0026cb009c
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-08 18:19:47 +0300

    [new]:mock a http call and assert calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,13 +2,14 @@
 """
 Unit test for utils functions
 """
-from unittest import TestCase
+import unittest
+from unittest import mock
 from parameterized import parameterized
 
-access_nested_map = __import__('utils').access_nested_map
+utils = __import__('utils')
 
 
-class TestAccessNestedMap(TestCase):
+class TestAccessNestedMap(unittest.TestCase):
     """
     A test class for access_nested_map function
     """
@@ -20,7 +21,7 @@ class TestAccessNestedMap(TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """A test method for access_nested_map function
         """
-        self.assertEqual(access_nested_map(nested_map, path), expected)
+        self.assertEqual(utils.access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
         ({}, ("a",)),
@@ -30,4 +31,24 @@ class TestAccessNestedMap(TestCase):
         """Test method to check whether exception is raised
         """
         with self.assertRaises(KeyError):
-            access_nested_map(nested_map, path)
+            utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """A Test class for the get_json method
+    """
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+        ])
+    @mock.patch('utils.requests')
+    def test_get_json(self, url, payload, mock_requests):
+        """A method implemented to test the get_json function
+        """
+        mock_response = mock.Mock()
+        mock_response.json.return_value = payload
+
+        mock_requests.get.return_value = mock_response
+
+        self.assertEqual(utils.get_json(url), payload)
+        mock_requests.get.assert_called_once()
