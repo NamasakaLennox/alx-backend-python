940567a6d2f934ad647ba3e34b433c575b4b6c1b
NamasakaLennox <namasakalennox254@gmail.com>
2024-01-08 17:04:30 +0300

    [new]: check whether exception is raised with given parameterized input

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -21,3 +21,13 @@ class TestAccessNestedMap(TestCase):
         """A test method for access_nested_map function
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+        ])
+    def test_access_nested_map_exception(self, nested_map, path):
+        """Test method to check whether exception is raised
+        """
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
